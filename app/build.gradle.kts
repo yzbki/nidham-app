9cb755c0bb6001134d98f1189579c3ee0faea8bf
Mustafa Al-Youzbaki <47333242+yzbki@users.noreply.github.com>
2026-07-24 22:15:59 -0400

    Update assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.youzbaki.nidham"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.0"
+        versionCode = 9
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
